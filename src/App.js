222fa0e6de186372a967935d83d062517263599d
WebMobileDevelopment <maximmironenkoreactu@example.com>
2022-02-21 10:08:23 +0900

    coding style fixed

--- src/App.js
+++ src/App.js
@@ -4,24 +4,30 @@ import Web3 from 'web3';
 import { CONTACT_ABI, CONTACT_ADDRESS } from './contracts/simpleStorage';
 
 function App() {
+  //----- classes ------
   const classes = useStyles();
 
-  const [value, setValue] = useState(0);
-  const [num, setNum] = useState(0);
+  //----- Internal States -----
+  const [account, setAccount] = useState(); // My account
+  const [storage, setStorage] = useState(); // storage that the contract made
+  const [value, setValue] = useState(); // The value that stored in the Storage
+  const [num, setNum] = useState(); // Inputed value
 
+  //----- Custom Functions -----
+  // Set Inputed Value To the Storage
   const setValueToStorage = async () => {
     await storage.methods.set(num).send({from: account});
   }
 
+  // Get Value From the Storage and Display on the Page
   const getValueFromStorage = async () => {
     var val = await storage.methods.get().call();
     setValue(val);
   }
 
-  const [account, setAccount] = useState();
-  const [storage, setStorage] = useState();
-
+  //----- Lifecycle Events -----
   useEffect(() => {
+    // Web3.js initiate
     async function load() {
       const web3 = new Web3(Web3.givenProvider || 'https://ropsten.infura.io/v3/e5f6b05589544b1bb8526dc3c034c63e');
       const accounts = await web3.eth.requestAccounts();
@@ -39,7 +45,7 @@ function App() {
       <input type='number' placeholder='number' onChange={(e) => setNum(e.target.value)} />
       <br/><br/>
       <button className={classes.button} onClick={setValueToStorage}>SET</button>
-      <h4> {value} </h4>
+      <h4> The Value is : {value} </h4>
       <button className={classes.button} onClick={getValueFromStorage}>GET</button>
     </div>
   );
@@ -49,8 +55,9 @@ const useStyles = makeStyles((theme) => ({
   button: {
     width: 100,
     height: 50,
-    backgroundColor: 'white',
-    color: 'blue'
+    backgroundColor: 'teal',
+    borderRadius: 5,
+    color: 'white'
   }
 }))
 
